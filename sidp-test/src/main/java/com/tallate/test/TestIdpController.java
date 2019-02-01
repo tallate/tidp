a28e130e7123e7ec8d18a043d417285cb663696b
huanggaochi <huanggaochi@benmu-health.com>
2019-02-01 20:13:18 +0800

    hi

--- sidp-test/src/main/java/com/tallate/test/TestIdpController.java
+++ sidp-test/src/main/java/com/tallate/test/TestIdpController.java
@@ -2,6 +2,7 @@ package com.tallate.test;
 
 import com.tallate.sidp.EnableIdp;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,33 +13,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test/idp")
 public class TestIdpController {
 
-  @EnableIdp
-  @RequestMapping("/hello")
-  public String hello() {
-    return "Hello";
-  }
+    public static enum MsgType {
+        A, B
+    }
 
-  @EnableIdp
-  @RequestMapping("/spanId")
-  public String spanId() throws InterruptedException {
-    System.out.println(">> 请求处理中...");
-    Thread.sleep(1000);
-    System.out.println("<< 请求处理完毕...");
-    return "OK";
-  }
+    @RequestMapping("/hi")
+    public MsgType hi(@RequestParam("msg") String msg, @RequestParam("msgType") MsgType msgType) {
+        return msgType;
+    }
 
-  @EnableIdp
-  @RequestMapping("/throwEx")
-  public String throwEx() throws Exception {
-    System.out.println(">> 请求处理中...");
-    throw new Exception("故意抛出的Exception");
-  }
+    @EnableIdp
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
 
-  @EnableIdp
-  @RequestMapping("/throwRuntimeEx")
-  public String throwRuntimeEx() {
-    System.out.println(">> 请求处理中...");
-    throw new RuntimeException("故意抛出的RuntimeException");
-  }
+    @EnableIdp
+    @RequestMapping("/spanId")
+    public String spanId() throws InterruptedException {
+        System.out.println(">> 请求处理中...");
+        Thread.sleep(1000);
+        System.out.println("<< 请求处理完毕...");
+        return "OK";
+    }
+
+    @EnableIdp
+    @RequestMapping("/throwEx")
+    public String throwEx() throws Exception {
+        System.out.println(">> 请求处理中...");
+        throw new Exception("故意抛出的Exception");
+    }
+
+    @EnableIdp
+    @RequestMapping("/throwRuntimeEx")
+    public String throwRuntimeEx() {
+        System.out.println(">> 请求处理中...");
+        throw new RuntimeException("故意抛出的RuntimeException");
+    }
 
 }
