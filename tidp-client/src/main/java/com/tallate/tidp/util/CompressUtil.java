d037acda278b9bf66c0d70f4d6165660cb7e8e34
tallate <787985369@qq.com>
2019-03-12 02:03:15 +0800

    init

--- tidp-client/src/main/java/com/tallate/tidp/util/CompressUtil.java
+++ tidp-client/src/main/java/com/tallate/tidp/util/CompressUtil.java
@@ -8,23 +8,49 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 /**
  * 数据压缩
+ * 1. gzip和zip，gzip相对来说压缩比率更高
  *
  * @author hgc
  * @date 1/30/19
  */
 public class CompressUtil {
 
+  public static byte[] gzip(String str) throws IOException {
+    if (Strings.isNullOrEmpty(str)) {
+      return new byte[0];
+    }
+    return compressByGZIP(str, "UTF-8");
+  }
+
+  public static byte[] compressByGZIP(String str, String encoding) throws IOException {
+    try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+         GZIPOutputStream gzip = new GZIPOutputStream(out)) {
+      gzip.write(str.getBytes(encoding));
+      return out.toByteArray();
+    }
+  }
+
   /**
    * 使用zip格式解压 1. 默认使用UTF-8编码
    */
-  public static String unZip(byte[] bytes) throws IOException {
+  public static String unGZIP(byte[] bytes) throws IOException {
     if (null == bytes || 0 == bytes.length) {
       return "";
     }
-    return extByZip(bytes, "UTF-8");
+    return extByGZIP(bytes, "UTF-8");
+  }
+
+  private static String extByGZIP(byte[] bytes, String encoding) throws UtilException, IOException {
+    String ret;
+    try (GZIPInputStream gi = new GZIPInputStream(new ByteArrayInputStream(bytes))) {
+      ret = FileUtil.readStream(gi, encoding);
+    }
+    return ret;
   }
 
   /**
@@ -38,19 +64,29 @@ public class CompressUtil {
   }
 
   /**
+   * 使用zip格式解压 1. 默认使用UTF-8编码
+   */
+  public static String unZip(byte[] bytes) throws IOException {
+    if (null == bytes || 0 == bytes.length) {
+      return "";
+    }
+    return extByZip(bytes, "UTF-8");
+  }
+
+  /**
    * 使用zip格式压缩 1. 需要指定编码
    */
   private static byte[] compressByZip(String str, String encoding) throws IOException {
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    try (GZIPOutputStream gzip = new GZIPOutputStream(out)) {
+    try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+         ZipOutputStream gzip = new ZipOutputStream(out)) {
       gzip.write(str.getBytes(encoding));
+      return out.toByteArray();
     }
-    return out.toByteArray();
   }
 
   private static String extByZip(byte[] bytes, String encoding) throws UtilException, IOException {
     String ret;
-    try (GZIPInputStream gi = new GZIPInputStream(new ByteArrayInputStream(bytes))) {
+    try (ZipInputStream gi = new ZipInputStream(new ByteArrayInputStream(bytes))) {
       ret = FileUtil.readStream(gi, encoding);
     }
     return ret;
